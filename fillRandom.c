b6d4568e54c95cd92823c04a244c9f718fd6f2f3
Meriem Soubih <soubih.meriem@gmail.com>
2023-10-12 19:18:21 +0100

    .

---
+++ fillRandom.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void fillRandom(int *tab, int size, int max)
+{
+}
+
+int main()
+{
+    int A[] = {20, 13, 23, 22, 30, 49, 45, 21, 56, 9};
+    int x;
+    scanf("%d", &x);
+    printf("%d \n", x);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d \n", A[i]);
+    }
+    
+}
